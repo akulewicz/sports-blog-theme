5efbf9ae016c7d73035263d1647a80fbe2e895df
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-05-25 20:27:11 +0200

    Fix iframe styles.

--- content-page.php
+++ content-page.php
@@ -1,6 +1,6 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<article class="page">
-    <header class="page__header">
+<article class="post">
+    <header class="post__header">
         <h1 class="post__title post__title--big"><?= the_title(); ?></h1>
         <p class="post__date post__date--with-margin"><?= get_the_date(); ?></p>
     </header>
