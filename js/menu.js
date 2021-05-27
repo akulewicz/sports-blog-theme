eab885b08f487c52961ccdfdb9840e48ff423219
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-05-27 22:03:13 +0200

    Fix displaying menu on mobile devices.

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
 const navigationSwitcher = document.querySelector('.top-navigation__switcher--js');
 
 navigationSwitcher.addEventListener('click', (e) => {
-    const navigationList = document.querySelector('.top-navigation__list--js');
-    navigationList.classList.toggle('top-navigation__list--visible');
-    if (navigationList.classList.contains('top-navigation__list--visible')) {
+    const navigation = document.querySelector('.top-navigation--js');
+    navigation.classList.toggle('top-navigation--visible');
+    if (navigation.classList.contains('top-navigation--visible')) {
         navigationSwitcher.innerHTML = '<i class="fas fa-times"></i>';
     } else {
         navigationSwitcher.innerHTML = '<i class="fas fa-bars"></i>';
